626939aa537eff750e4b70e6e336ee08a782c8f5
Sangwook-Ahn <tkddnr924@naver.com>
2016-12-09 00:28:27 +0900

    distribution update

--- mysite/twpoint/static/twpoint/data/distribution-data.js
+++ mysite/twpoint/static/twpoint/data/distribution-data.js
@@ -16,86 +16,31 @@ $(function(){
             zoomControl: true,
         });
 
+        var locations = [];
 
-        var data = {
-            type: "FeatureCollection",
-            metadata: {
-                title: "Way Location",
-                api: "1.0.13",
-                status: 200,
-                count: 0,
-            },
-            features:[],
-            bbox: [
-                32.047978,
-                132.160925,
-                -3,
-                38.538825,
-                132.015932,
-                609.13
-            ]
-        };
-        var feature = createFeature();
-        for ( var i=0; i<way_data.length; i++) {
-            if(way_data[i].position != null && way_data[i].position_x != null && way_data[i].position_y != null){
-                feature.geometry.coordinates.push(Number(way_data[i].position_x));
-                feature.geometry.coordinates.push(Number(way_data[i].position_y));
-                feature.geometry.coordinates.push(7);
-
-                feature.id = way_data[i].id;
-                data.features.push(feature);
-                feature = createFeature();
+        way_data.forEach(function(item, index, array){
+            var location;
+            if(item.position != null && item.position_x != null && item.position_y != null){
+                location = {lat: Number(item.position_y), lng: Number(item.position_x)};
+                locations.push(location);
+            } else if (item.start != null && item.start_x != null && item.start_y != null){
+                location = {lat: Number(item.start_y), lng: Number(item.start_x)};
+                locations.push(location);
+            } else if (item.search != null && item.search_x != null && item.search_y != null){
+                location = {lat: Number(item.search_y), lng: Number(item.search_x)};
+                locations.push(location);
             }
-            else if(way_data[i].start != null && way_data[i].start_x != null && way_data[i].start_y != null){
-                feature.geometry.coordinates.push(Number(way_data[i].start_x));
-                feature.geometry.coordinates.push(Number(way_data[i].start_y));
-                feature.geometry.coordinates.push(7);
 
-                feature.id = way_data[i].id;
-                data.features.push(feature);
-                feature = createFeature();
-            }
-            else if(way_data[i].search != null && way_data[i].search_x != null && way_data[i].search_y != null){
-                feature.geometry.coordinates.push(Number(way_data[i].search_x));
-                feature.geometry.coordinates.push(Number(way_data[i].search_y));
-                feature.geometry.coordinates.push(7);
-
-                feature.id = way_data[i].id;
-                data.features.push(feature);
-                feature = createFeature();
-            }
-        }
-        data.metadata.count = data.features.length;
-        d_map.data.setStyle(function(feature) {
-          var magnitude = 5;
-          return {
-            icon: getCircle(magnitude)
-          };
         });
-        d_map.data.addGeoJson(data);
 
+
+        var markers = locations.map(function(location, i) {
+            return new google.maps.Marker({
+                position: location,
+            });
+        });
+
+        var markerCluster = new MarkerClusterer(d_map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
   });
 })
-
-function createFeature(){
-    return {
-        type: "Feature",
-        geometry: {
-            type: "Point",
-            coordinates:[
-            ]
-        },
-        id: "",
-    };
-}
-
-function getCircle(magnitude) {
-        return {
-          path: google.maps.SymbolPath.CIRCLE,
-          fillColor: 'red',
-          fillOpacity: 0.18,
-          scale: Math.pow(2, magnitude) / 2,
-          strokeColor: 'white',
-          strokeWeight: .5
-        };
-      }
