ed75bf6772ccbd168641967aa16fc02a27169dd7
Sangwook-Ahn <tkddnr924@naver.com>
2017-09-13 02:52:23 +0900

    mysite/mysite/urls.py 수정

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
+from django.conf.urls.static import static, serve
 from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('twpoint.urls', namespace="twpoint")),
-    # url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-]
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
+
+] + static(settings.MEDIA_URL, document_url=settings.MEDIA_ROOT)
